8becdcd0d2e4a8cadbcfc0ce30966ef5a79fe342
Devon Jones <linesbydevon@gmail.com>
2022-12-08 14:11:22 -0500

    added config back

--- server/routes/AssignmentRouter.js
+++ server/routes/AssignmentRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express').Router()
 const controller = require('../controllers/AssignmentController')
 
-Router.get('/', controller.FindAllAssignments);
+
 Router.get('/:student_id', controller.FindAllAssignmentsByStudent);
 Router.post('/:class_id', controller.CreateAssignment);
 Router.put('/:assignment_id', controller.UpdateAssignment);
 Router.delete('/:assignment_id', controller.DeleteAssignment);
+Router.get('/', controller.FindAllAssignments);
 
 module.exports = Router
